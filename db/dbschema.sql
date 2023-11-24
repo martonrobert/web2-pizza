5a2be7986334b8b61f98621201cdbf6cacdebfec
martonrobert <shxmtu@hallgato.uni-neumann.hu>
2023-11-24 16:22:49 +0100

    db schema

---
+++ db/dbschema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE pizza
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+use pizza;
+
+create table pizza (
+    nev varchar(255) not null,
+    kategorianev varchar(255) not null,
+    vegetarianus tinyint(4) not null default 0,
+    primary key (nev)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Pizzák';
+
+create table kategoria (
+    nev varchar(255) not null,
+    ar DECIMAL(12,2) not null,
+    primary key (nev)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Pizza kategóriák';
+
+
+create table rendeles (
+    az int(10) unsigned not null auto_increment,
+    pizzanev varchar(255) not null,
+    darab int(10) unsigned not null,
+    felvetel date not null,
+    kiszallitas date not null,
+    primary key (az)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Pizza kategóriák';
+
