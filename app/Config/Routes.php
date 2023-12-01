191f251fae974246ae15bc6ce8a61334aa4947e6
martonrobert <shxmtu@hallgato.uni-neumann.hu>
2023-12-01 18:49:25 +0100

    config

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,3 +5,28 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+/**
+ * Főoldal
+ */
+$routes->get('/', 'HomePageController::index');
+
+/**
+ * Egy konkrét pizza adatainak lekérdezése
+ */
+$routes->get('pizza/(:any)', 'PizzaController::getPizza/$1');
+
+/**
+ * Új pizza rendelés felület
+ */
+$routes->get('order-pizza/(:any)', 'OrderController::newOrder/$1');
+
+/**
+ * Pizza rendelés rögzítése
+ */
+$routes->post('orders', 'OrderController::setOrder');
+
+/**
+ * Rendelések listája
+ */
+$routes->get('orders', 'OrderController::getOrdersList');
