e76094748a14dcff2f0d24f76d4431bf1c8019f1
martonrobert <shxmtu@hallgato.uni-neumann.hu>
2024-01-20 22:37:47 +0100

    db user kezeles

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -26,3 +26,34 @@ create table rendeles (
     primary key (az)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Pizza rendelések';
 
+
+
+drop table if exists felhasznalok;
+create table felhasznalok (
+    id int(10) unsigned not null auto_increment,
+    nev varchar(255) not null,
+    adminisztrator tinyint(4) unsigned not null default 0,
+    email varchar(255) not null comment 'E-mail cím, egyben login név is',
+    telefon varchar(20),
+    kod varchar(60),
+    jelszo varchar(255) not null,
+    regisztracio datetime not null,
+    email_megerosítve datetime,
+    allapot varchar(1) not null comment 'Állapot: A: aktív, D: törölt',
+    primary key (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Felhasználók';
+
+insert into felhasznalok values (1,'',0,'',null,null,'','2000-01-01 00:00:00',null,'D');
+
+drop table if exists tokens;
+create table tokens (
+    id int(10) unsigned not null auto_increment,
+    felhasznalo_id int(10) unsigned not null,
+    token_str varchar(255) not null,
+    letrehozva datetime not null,
+    torolt tinyint(4) not null default 0,
+    primary key (id),
+    index (token_str)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci COMMENT='Felhasználók';
+
+insert into tokens values (1,1,'','2000-01-01 00:00:00',1);
